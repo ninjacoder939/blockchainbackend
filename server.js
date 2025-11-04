0cd98eaaa3f798498a75b5b6aae32e52c88f7fd9
ninjacoder <ckulthe56@gmail.com>
2025-11-04 14:27:59 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
+
 app.use(express.json());
 
 // MongoDB connection
